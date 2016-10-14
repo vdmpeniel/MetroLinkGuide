df44a25f64615c3438b4e1915bd9a3c80c693bd5
victor diaz <vdmpeniel@yahoo.es>
2016-10-14 04:13:06 -0400

    Eureka Spring Annotations correct folder(last one got mixed with wrong files)

--- JavaData/src/main/java/DataBaseAccess/DataBaseAccess.java
+++
@@ -1,8 +0,0 @@
-package DataBaseAccess;
-
-import java.util.List;
-
-public interface DataBaseAccess {
-    List getStations();
-    List getArrivals(String userStation, String time);
-}
--- JavaData/src/main/java/Timer/Timer.java
+++
@@ -1,7 +0,0 @@
-package Timer;
-import java.time.LocalTime;
-
-public interface Timer {
-    LocalTime getTime();
-    Long getTimeToNext(String nextArrival);
-}
